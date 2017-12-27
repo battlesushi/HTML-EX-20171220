d1b3807a4c6e68170ef58df2449fb5963f99997f
battlesushi <105021030@live.asia.edu.tw>
2017-12-27 12:05:00 +0800

    1227

--- loginCheck.php
+++ loginCheck.php
@@ -12,14 +12,17 @@ $pw = sha1($_POST['pwd']);
 $sql = "SELECT * FROM userList where username = '$id'";
 $result = mysqli_query($link,$sql);
 $row = @mysqli_fetch_row($result);
-
+$email=$row[3];
 //判斷帳號與密碼是否為空白
 //以及MySQL資料庫裡是否有這個會員
 if($id != null && $pw != null && $row[1] == $id && $row[2] == $pw)
 {
         //將帳號寫入session，方便驗證使用者身份
+        $sql = "insert into log (email, event_type, description) values ('$email','login','user login success from".get_client_ip()."')";
+        $link->query($sql);
         $_SESSION['account'] = $id;
         echo '登入成功!';
+//        require_once('mailTest.php');//<--有人登入之後 寄信管理者信箱
         echo '<meta http-equiv=REFRESH CONTENT=1;url=admindex.php>';
 }
 else
@@ -28,3 +31,23 @@ else
         echo '<meta http-equiv=REFRESH CONTENT=1;url=login.html>';
 }
 ?>
+<?php
+// Function to get the client IP address
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+        $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
--- mailTest.php
+++ mailTest.php
@@ -1,7 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: USER
- * Date: 2017/12/27
- * Time: 上午 10:17
- */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'battlesushi@gmail.com';
+    $mail->Password = '0939795377';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+
+    $mail->setFrom('battlesushi@gmail.com', 'Admin');
+//    $mail->addAddress('recipient1@example.net', 'Recipient1');
+    $mail->addAddress('z27850457@gmail.com');  //收件者
+
+    $mail->addReplyTo('noreply@example.com', 'noreply');
+//    $mail->addCC('cc@example.com');
+//    $mail->addBCC('bcc@example.com');
+
+    //Attachments
+//    $mail->addAttachment('/backup/myfile.tar.gz');
+
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Mail Subject!';
+    $mail->Body    = 'This is SMTP Email Test';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
